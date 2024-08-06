f7ff73c15b337997d5042fdf8e2892eb8b1ea658
RouSage <kolbasovsergey02@gmail.com>
2024-08-06 22:11:45 +0200

    feat: read watchlist from csv

---
+++ internal/watchlist/reader.go
@@ -0,0 +1,69 @@
+package watchlist
+
+import (
+	"encoding/csv"
+	"io"
+	"log"
+	"time"
+)
+
+const (
+	CREATED      = 2
+	TITLE        = 5
+	URL          = 7
+	TYPE         = 8
+	RELEASE_DATE = 14
+)
+
+type Watchlist struct {
+	Title       string
+	URL         string
+	Created     time.Time
+	Type        string
+	ReleaseDate time.Time
+}
+
+func ReadWatchlist(file io.Reader) []Watchlist {
+	reader := csv.NewReader(file)
+	reader.FieldsPerRecord = 0
+
+	data, err := reader.ReadAll()
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	var watchlist []Watchlist
+
+	for i, line := range data {
+		// Skip the header row
+		if i == 0 {
+			continue
+		}
+
+		item := Watchlist{}
+		for j, field := range line {
+			switch j {
+			case CREATED:
+				item.Created, err = time.Parse("2006-01-02", field)
+				if err != nil {
+					log.Fatalln(err)
+				}
+			case TITLE:
+				item.Title = field
+			case URL:
+				item.URL = field
+			case TYPE:
+				item.Type = field
+			case RELEASE_DATE:
+				item.ReleaseDate, err = time.Parse("2006-01-02", field)
+				if err != nil {
+					log.Fatalln(err)
+				}
+			}
+		}
+
+		watchlist = append(watchlist, item)
+	}
+
+	return watchlist
+}
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+	"time"
+	"watchlist-exporter/internal/watchlist"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	file, err := os.Open("./data/movies.csv")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	defer file.Close()
+
+	parsedWatchlist := watchlist.ReadWatchlist(file)
+	fmt.Printf("%+v", parsedWatchlist)
+	fmt.Println(parsedWatchlist[0].Created.Format(time.DateOnly))
 }
